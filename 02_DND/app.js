e6995cb8641255d802655c88d49c008c35af33f5
alexandr <zbc@mail.ru>
2021-06-15 22:36:28 +0300

    initial commit

---
+++ 02_DND/app.js
@@ -0,0 +1,49 @@
+function dragStart(e) {
+    e.target.classList.add('hold')
+    setTimeout(() => {
+        e.target.classList.add('hide')
+    }, 0)
+}
+
+function dragEnd(e) {
+    e.target.className = 'item'
+}
+
+
+function dragOver(e) {
+    e.preventDefault()
+    // e.target.classList.add('')
+}
+
+function dragEnter(e) {
+    e.target.classList.add('hovered')
+}
+
+function dragLeave(e) {
+    e.target.classList.remove('hovered')
+}
+
+function dragDrop(e) {
+    e.target.classList.remove('hovered')
+    e.target.append(item)
+
+}
+
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragOver)
+    placeholder.addEventListener('dragenter', dragEnter)
+    placeholder.addEventListener('dragleave', dragLeave)
+    placeholder.addEventListener('drop', dragDrop)
+}
+
+item.addEventListener('dragstart', dragStart)
+item.addEventListener('dragend', dragEnd)
+
+
+
+
+
+
