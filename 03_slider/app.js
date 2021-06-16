4aa273b0232f346ad6b0563865ac49a414315f60
alexandr <zbc@mail.ru>
2021-06-16 16:06:14 +0300

    Презентационный слайдер с анимацией. Красивый :-)

---
+++ 03_slider/app.js
@@ -0,0 +1,25 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
+
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+
+let activeSlideIndex = 0
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+
+
+function changeSlide(direction) {
+    direction === 'up' ? activeSlideIndex = ++activeSlideIndex % slidesCount : true
+    direction === 'down' ? activeSlideIndex ? --activeSlideIndex : activeSlideIndex = slidesCount - 1 : true
+    const height = container.clientHeight
+    console.log(activeSlideIndex, height)
+    mainSlide.style.transform = `translateY(-${(activeSlideIndex) * height}px)`
+    sidebar.style.transform = `translateY(${(activeSlideIndex) * height}px)`
+}
+
+upBtn.addEventListener('click', () => changeSlide('up'))
+downBtn.addEventListener('click', () => changeSlide('down'))
